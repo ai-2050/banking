fa437b1e6714cd4fe6cec929b9c40a32479ff239
aleksander <deephorizon.ai.2050@gmail.com>
2024-06-26 11:47:40 +0300

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,38 @@
 import React from "react";
-
+import HeaderBox from "@/components/ui/HeaderBox";
+import TotalBalanceBox from "@/components/ui/TotalBalanceBox";
+import RightSidebar from "@/components/ui/RightSidebar";
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = {
+    firstName: "Aleksander",
+    lastName: "JSM",
+    email: "contact@jsmatery.pro",
+  };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500.0 }]}
+      />
+    </section>
+  );
 };
 
 export default Home;
